4386b75e01807d6e3f68934397fc53425eb52002
Lex van Os <vanoslex7@gmail.com>
2023-10-14 14:04:03 +0200

    Added authentication to the application through Breeze

--- VirtueVerse/resources/views/app.blade.php
+++ VirtueVerse/resources/views/app.blade.php
@@ -9,10 +9,10 @@
     <!-- Navbar -->
     <nav class="bg-zinc-300 p-4">
         <div class="container mx-auto flex flex-wrap justify-between items-center">
-            <div class="w-full lg:w-3/4 mb-4 lg:mb-0">
+            <div class="w-full lg:w-3/5 mb-4 lg:mb-0">
                 <a href="/" class="text-black text-2xl font-bold">VirtueVerse</a>
             </div>
-            <div class="w-full lg:w-1/4 space-x-4 lg:space-x-8 flex items-center lg:justify-end">
+            <div class="w-full lg:w-2/5 space-x-4 lg:space-x-8 flex items-center lg:justify-end">
                 <a href="/" class="text-black hover:underline">Home</a>
                 <a href="/book/catalogue" class="text-black hover:underline">Book Catalogue</a>
                 <div class="relative group">
@@ -28,6 +28,19 @@
                         <li><a href="/book-edition/create" class="px-4 py-2">Create Book Edition</a></li>
                         <li><a href="/author/create" class="px-4 py-2">Create Author</a></li>
                     </ul>
+                </div>
+                <div class="flex space-x-4">
+                    @guest
+                        <a href="{{ route('login') }}" class="text-black hover:underline">Login</a>
+                        <a href="{{ route('register') }}" class="text-black hover:underline">Register</a>
+                    @endguest
+                    @auth
+                        <a href="{{ route('profile.edit') }}" class="text-black hover:underline">Profile</a>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" class="text-black hover:underline">Logout</a>
+                        </form>
+                    @endauth
                 </div>
             </div>
             <div class="lg:hidden">
@@ -46,16 +59,16 @@
     <div class="footer-layout px-48 bg-zinc-300">
         <footer class="text-black py-8">
             <div class="container mx-auto flex flex-wrap justify-between">
-                <div class="footer-left w-full lg:w-3/4 mb-4 lg:mb-0"> <!-- Adjusted width and added margin -->
+                <div class="footer-left w-full lg:w-4/5 mb-4 lg:mb-0"> <!-- Adjusted width and added margin -->
                     <div class="flex flex-wrap">
-                        <div class="w-full md:w-1/2 lg:w-1/3 mb-6 lg:mb-0"> <!-- Adjusted class for Discover items -->
+                        <div class="w-full md:w-1/2 lg:w-1/4 mb-6 lg:mb-0"> <!-- Adjusted class for Discover items -->
                             <h2 class="text-2xl font-semibold mb-4">About</h2>
                             <ul class="text-sm">
                                 <li><a href="" class="hover:underline">About VirtueVerse</a></li>
                                 <li><a href="" class="hover:underline">Privacy Policy</a></li>
                             </ul>
                         </div>
-                        <div class="w-full md:w-1/2 lg:w-1/3 mb-6 lg:mb-0"> <!-- Adjusted class for Discover items -->
+                        <div class="w-full md:w-1/2 lg:w-1/4 mb-6 lg:mb-0"> <!-- Adjusted class for Discover items -->
                             <h2 class="text-2xl font-semibold mb-4">Discover</h2>
                             <ul class="text-sm">
                                 <li><a href="/book/catalogue" class="hover:underline">Books</a></li>
@@ -63,16 +76,30 @@
                                 <li><a href="/book-edition/create" class="hover:underline">Book Editions</a></li>
                             </ul>
                         </div>
-                        <div class="w-full md:w-1/2 lg:w-1/3">
+                        <div class="w-full md:w-1/2 lg:w-1/4">
                             <h2 class="text-2xl font-semibold mb-4">Get in Touch</h2>
                             <ul class="text-sm">
                                 <li><a href="" class="hover:underline" target="_blank">Portfolio</a></li>
                                 <li><a href="https://github.com/Lex-van-Os/VirtueVerse" class="hover:underline" target="_blank">GitHub</a></li>
                             </ul>
                         </div>
+                        <div class="w-full md:w-1/2 lg:w-1/4">
+                            <h2 class="text-2xl font-semibold mb-4">Profile</h2>
+                            @guest
+                                <ul class="text-sm">
+                                    <li><a href="{{ route('login') }}" class="hover:underline">Login</a></li>
+                                    <li><a href="{{ route('register') }}" class="hover:underline">Register</a></li>
+                                </ul>
+                            @endguest
+                            @auth
+                                <ul class="text-sm">
+                                    <li><a href="{{ route('profile.edit') }}" class="hover:underline">My Profile</a></li>
+                                </ul>
+                            @endauth
+                        </div>
                     </div>
                 </div>
-                <div class="footer-right w-full lg:w-1/4">
+                <div class="footer-right w-full lg:w-1/5">
                     <p class="text-sm text-center lg:text-right">© VirtueVerse. All Rights Reserved.</p>
                 </div>
             </div>
--- VirtueVerse/routes/web.php
+++ VirtueVerse/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\BookEditionController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,25 +21,45 @@ use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');;
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    // Profile routes
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Book routes
+    Route::get('book/create', [BookController::class, 'create'])->name('book.create');
+    Route::post('book/store', [BookController::class, 'store'])->name('book.store')->middleware('web');
+
+    // Author routes
+    Route::get('author/create', [AuthorController::class, 'create'])->name('author.create');
+    Route::post('author/store', [AuthorController::class, 'store'])->name('author.store')->middleware('web');
+
+    // Book edition routes
+    Route::get('book-edition/create', [BookEditionController::class, 'create'])->name('book-edition.create');
+    Route::post('book-edition/store', [BookEditionController::class, 'store'])->name('book-edition.store')->middleware('web');
+});
+
+// Routes for registration
+require __DIR__.'/auth.php';
 
 // Route::get('book/create', 'BookController@create')->name('book.create');
-Route::get('book/create', [BookController::class, 'create'])->name('book.create');
 Route::get('book/catalogue', [BookController::class, 'catalogue'])->name('book.catalogue');
 Route::get('/book/{id}', [BookController::class, 'show'])->name('book.show');
-Route::post('book/store', [BookController::class, 'store'])->name('book.store')->middleware('web');
 Route::get('/book/search', [BookController::class, 'search'])->name('book.search');
 Route::get('book/searchStoredBooks', [BookController::class, 'searchStoredBooks'])->name('book.searchStoredBooks');
 Route::get('book/getBookInfo', [BookController::class, 'getBookInfo'])->name('book.getBookInfo');
 Route::get('book/getBook', [BookController::class, 'getBook'])->name('book.getBook');
 
-Route::get('author/create', [AuthorController::class, 'create'])->name('author.create');
-Route::post('author/store', [AuthorController::class, 'store'])->name('author.store')->middleware('web');
 Route::get('/author/search', [AuthorController::class, 'search'])->name('author.search');
 Route::get('author/getAuthorInfo', [AuthorController::class, 'getAuthorInfo'])->name('author.getAuthorInfo');
 
-Route::get('book-edition/create', [BookEditionController::class, 'create'])->name('book-edition.create');
-Route::post('book-edition/store', [BookEditionController::class, 'store'])->name('book-edition.store')->middleware('web');
 Route::get('/book-edition/search', [BookEditionController::class, 'search'])->name('book-edition.search');
 Route::get('/book-edition/getBookEditions', [BookEditionController::class, 'getBookEditions'])->name('book-edition.getBookEditions');
 
