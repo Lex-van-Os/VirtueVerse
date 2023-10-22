dfb8d0550d9cef4e77454bb5dcdd21c5387bcb36
Lex van Os <vanoslex7@gmail.com>
2023-10-22 20:45:36 +0200

    Created seeders and tweaked some database fields

---
+++ VirtueVerse/database/seeders/BookEditionsTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BookEdition;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookEditionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        BookEdition::create([
+            'title' => 'The "Summa theologica" of St. Thomas Aquinas',
+            'isbn' => '',
+            'language' => 'English',
+            'publication_year' => 1912,
+            'book_id' => 8, 
+            'editions_key' => '',
+            'pages' => 683,
+        ]);
+
+        BookEdition::create([
+            'title' => 'The Screwtape Letters',
+            'isbn' => '9780020867401',
+            'language' => 'English',
+            'publication_year' => 1982,
+            'book_id' => 1, 
+            'editions_key' => '',
+            'pages' => 172,
+        ]);
+
+        BookEdition::create([
+            'title' => 'The Screwtape Letters',
+            'isbn' => '9780060652937',
+            'language' => 'English',
+            'publication_year' => 2023,
+            'book_id' => 1, 
+            'editions_key' => '',
+            'pages' => 175,
+        ]);
+    }
+}
