1fdc4608d9cfe85e02271647d590f1b99cabb5f3
Lex van Os <vanoslex7@gmail.com>
2023-11-08 20:30:18 +0100

    Template for calls

---
+++ VirtueVerse/app/Http/Controllers/InsightsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InsightsController extends Controller
+{
+    public function getBookRecommendations(Request $request)
+    {
+
+    }
+
+    public function getExpectedCompletionTime(Request $request)
+    {
+
+    }
+
+    public function getPopularBooks(Request $request)
+    {
+        
+    }
+}
+
